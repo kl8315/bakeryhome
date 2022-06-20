022a9b20e0cf64e58971973d7dd834d8b3c3b058
kl8315 <kl0958@naver.com>
2022-06-20 21:44:23 +0900

    second commit

---
+++ Js/popup.js
@@ -0,0 +1,11 @@
+function partpopup() {
+    window.open("popup.html", "", "width=450, height=450");
+}
+
+function info() {
+    alert("DD 베이커리 홈페이지를 찾아주셔서 고맙습니다. \n종로에 4호점이 오픈되었습니다.\n많이 이용해주세요")
+}
+                    
+setTimeout(function () {
+    info();
+}, 3000);
